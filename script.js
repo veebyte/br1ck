97fbcf33b63a743a2bafb91ca3204a5324fb19ce
Byteeeeeeee <97357481+Byteeeeeeee@users.noreply.github.com>
2024-10-26 13:15:37 -0500

    Update script.js
    switched from alerting to having actual text
    added animation to said text
    changed step 12 -> 10 (i dont feel like updating variable names)

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function findTime(file) {
         const enrollmentStartIndex = enrollmentStart.indexOf('WARNING', 0);
         const startTime = enrollmentStart.substring(0, enrollmentStartIndex).trim();
 
-        const step12 = lines.find(line => line.includes('Step: 12'));
+        const step12 = lines.find(line => line.includes('Step: 10'));
         const timeIndex = step12.indexOf('WARNING', 0);
         const endTime = step12.substring(0, timeIndex).trim();
 
@@ -29,7 +29,12 @@ function findTime(file) {
 
         const timeDifference = (endDate - startDate) / 1000;
 
-        alert(`Difference: ${timeDifference} seconds`);
+        text = document.getElementById("reset-text")
+        text.innerText = "Reset Timing: " + timeDifference
+        text.style.display = "block"
+        text.style.animation = "glow 1.5s 1"
+        
+        setTimeout(() => {text.style.animation = "";}, 1500);
     };
 
     reader.readAsText(file);
