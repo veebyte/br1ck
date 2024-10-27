080a3e0d9c2bae994200756721baa60bfd09433e
Byteeeeeeee <97357481+Byteeeeeeee@users.noreply.github.com>
2024-10-27 11:09:20 -0500

    updating times and a tiny bit of css

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function findTime(file) {
         const enrollmentStartIndex = enrollmentStart.indexOf('WARNING', 0);
         const startTime = enrollmentStart.substring(0, enrollmentStartIndex).trim();
 
-        const step12 = lines.find(line => line.includes('Step: 10'));
+        const step12 = lines.find(line => line.includes('Blocking dev mode by device policy'));
         const timeIndex = step12.indexOf('WARNING', 0);
         const endTime = step12.substring(0, timeIndex).trim();
 
