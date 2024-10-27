9168aab47422ea11f74692b7dc9fb6e942d5c320
appleflyer <appleflyer@appleflyer-optiplex>
2024-10-28 02:04:54 +0800

    touchups + credits.

---
+++ tilt-card.js
@@ -0,0 +1,29 @@
+// slapped from bromine src code.
+// made by appleflyer. 2024.
+setTimeout(() => {
+    document.querySelectorAll('section').forEach(card => {
+        card.style.transition = 'transform 0.3s ease-out';
+        
+        let lastX = 0, lastY = 0;
+    
+        card.addEventListener('mousemove', e => {
+            const rect = card.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+    
+            if (lastX !== x || lastY !== y) {
+                lastX = x;
+                lastY = y;
+    
+                const xRotation = 5 * ((x - rect.width / 2) / rect.width);
+                const yRotation = -7 * ((y - rect.height / 2) / rect.height);
+                const rotation = `perspective(1000px) rotateX(${yRotation}deg) rotateY(${xRotation}deg)`;
+                card.style.transform = rotation;
+            }
+        });
+    
+        card.addEventListener('mouseleave', () => {
+            card.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg)`;
+        });
+    });
+}, 100);
